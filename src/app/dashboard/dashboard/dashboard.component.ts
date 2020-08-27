100d7fcb260f214da05134b436de02b2bcfa27a8
syedib <jebasingh.joshua@duckcreek.com>
2020-08-27 20:51:07 +0530

    last refreshed added

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -8,7 +8,6 @@ import { INFORMATION_WINDOW_ID } from 'src/app/actions/constant';
 declare var mxUtils: any;
 declare var mxCodec: any;
 declare var mxGraph: any;
-declare var mxGeometry: any;
 declare var mxConstants: any;
 
 @Component({
@@ -27,6 +26,7 @@ export class DashboardComponent implements AfterViewInit, OnInit {
   selectedClientId = 1;
   isAutoRefreshChecked = false;
   graphLoading = false;
+  refreshTime = '';
   cellStyleNotOnHover: string[] = ['JRmProH149STGmIK2Tsz-3', 'JRmProH149STGmIK2Tsz-1'];
   constructor(private hubService: HupService) {
     this.resourceModel = new ResourceModel();
@@ -45,97 +45,79 @@ export class DashboardComponent implements AfterViewInit, OnInit {
     this.hubService.getXml().subscribe(xmlString => {
       this.graphContainer.nativeElement.innerHTML = '';
       const graph = new mxGraph(this.graphContainer.nativeElement);
-      function updateStyle(state, hover)
-				{
-          if (state.cell && ['JRmProH149STGmIK2Tsz-3', 'JRmProH149STGmIK2Tsz-1'].indexOf(state.cell.id) < 0)
-          {
+      function updateStyle(state, hover) {
+          if (state.cell && ['JRmProH149STGmIK2Tsz-3', 'JRmProH149STGmIK2Tsz-1'].indexOf(state.cell.id) < 0) {
             state.style[mxConstants.STYLE_ROUNDED] = (hover) ? '1' : '0';
             state.style[mxConstants.STYLE_STROKEWIDTH] = (hover) ? '4' : '1';
             state.style[mxConstants.CURSOR_CONNECT] = (hover) ? 'pointer' : '';
           }
-				};
-				
+				}
+
 				// Changes fill color to red on mouseover
-				graph.addMouseListener(
+				  graph.addMouseListener(
 				{
 				    currentState: null,
 				    previousStyle: null,
-				    mouseDown: function(sender, me)
-				    {
-				        if (this.currentState != null)
-				        {
+				    mouseDown(me) {
+				        if (this.currentState != null) {
 				        	this.dragLeave(me.getEvent(), this.currentState);
 				        	this.currentState = null;
 				        }
 				    },
-				    mouseMove: function(sender, me)
-				    {
-				        if (this.currentState != null && me.getState() == this.currentState)
-				        {
+				    mouseMove(me) {
+				        if (this.currentState != null && me.getState() == this.currentState) {
 				            return;
 				        }
 
-				        var tmp = graph.view.getState(me.getCell());
+				        let tmp = graph.view.getState(me.getCell());
 
 				        // Ignores everything but vertices
 				        if (graph.isMouseDown || (tmp != null && !
-				            graph.getModel().isVertex(tmp.cell)))
-				        {
+				            graph.getModel().isVertex(tmp.cell))) {
 				        	tmp = null;
 				        }
 
-				        if (tmp != this.currentState)
-				        {
-				            if (this.currentState != null)
-				            {
+				        if (tmp != this.currentState) {
+				            if (this.currentState != null) {
 				                this.dragLeave(me.getEvent(), this.currentState);
 				            }
 
 				            this.currentState = tmp;
 
-				            if (this.currentState != null)
-				            {
+				            if (this.currentState != null) {
 				                this.dragEnter(me.getEvent(), this.currentState);
 				            }
 				        }
 				    },
-				    mouseUp: function(sender, me) { },
-				    dragEnter: function(evt, state)
-				    {
-				        if (state != null)
-				        {
+				    mouseUp() { },
+				    dragEnter(state) {
+				        if (state != null) {
 				        	this.previousStyle = state.style;
 				        	state.style = mxUtils.clone(state.style);
 				        	updateStyle(state, true);
-                  
-                  if (state && state.shape)
-                  {
+
+             if (state && state.shape) {
                     state.shape.apply(state);
-				        	  state.shape.redraw();
+				        	       state.shape.redraw();
                   }
-				        	
-				        	if (state.text != null)
-				        	{
+
+				        	if (state.text != null) {
 				        		state.text.apply(state);
 				        		state.text.redraw();
 				        	}
 				        }
 				    },
-				    dragLeave: function(evt, state)
-				    {
-				        if (state != null)
-				        {
+				    dragLeave(state) {
+				        if (state != null) {
 				        	state.style = this.previousStyle;
 				        	updateStyle(state, false);
-                  
-                  if (state && state.shape)
-                  {
+
+             if (state && state.shape) {
                     state.shape.apply(state);
-				        	  state.shape.redraw();
+				        	       state.shape.redraw();
                   }
-				        	
-				        	if (state.text != null)
-				        	{
+
+				        	if (state.text != null) {
 				        		state.text.apply(state);
 				        		state.text.redraw();
 				        	}
@@ -158,10 +140,10 @@ export class DashboardComponent implements AfterViewInit, OnInit {
           }
           elt = elt.nextSibling;
         }
-         graph.isHtmlLabel = (cell: any) => true;
+         graph.isHtmlLabel = () => true;
          graph.addCells(cells);
 
-         graph.selectCellForEvent = function(cell, evt) {
+         graph.selectCellForEvent = function(cell) {
 
           const informationWindow = cells.find(cell => cell.id  === INFORMATION_WINDOW_ID);
           console.log(cell);
@@ -179,7 +161,7 @@ export class DashboardComponent implements AfterViewInit, OnInit {
 
   ngAfterViewInit() {
     const graph = new mxGraph(this.graphContainer.nativeElement);
-    
+
     this.hubService.getXml().subscribe(xmlString => {
       try {
         const doc = mxUtils.parseXml(xmlString);
@@ -195,7 +177,7 @@ export class DashboardComponent implements AfterViewInit, OnInit {
           }
           elt = elt.nextSibling;
         }
-        graph.isHtmlLabel = (cell: any) => true;
+        graph.isHtmlLabel = () => true;
         graph.addCells(cells);
       } finally {
         graph.getModel().endUpdate();
@@ -208,21 +190,36 @@ export class DashboardComponent implements AfterViewInit, OnInit {
     this.getResourceByClientId(parseInt(event.target.value));
   }
 
-  autoRefreshOnChange(event) {
+  autoRefreshOnChange() {
     this.isAutoRefreshChecked = !this.isAutoRefreshChecked;
     if (!this.isAutoRefreshChecked) {
       this.updateSubscription.unsubscribe();
     } else { this.updateSubscription = interval(10000).subscribe(
-      (val) => { this.getResourceByClientId(this.selectedClientId);
+      () => { this.getResourceByClientId(this.selectedClientId);
     }); }
   }
 
   getResourceByClientId(clientId: number) {
     this.graphLoading = true;
     this.hubService.getResource(clientId).subscribe(response => {
+      this.refreshTime = this.getRefreshTime();
       this.graphLoading = false;
       this.resourceModel = response;
       this.setNetWorkGraph(response);
     });
+  }
+
+  getRefreshTime() {
+    // tslint:disable-next-line: one-variable-per-declaration
+    const currentTime = new Date(),
+      month = currentTime.getMonth() + 1,
+      day = currentTime.getDate(),
+      year = currentTime.getFullYear(),
+      hours = currentTime.getHours(),
+      minutes = currentTime.getMinutes(),
+      seconds = currentTime.getSeconds(),
+      text = (month + '/' + day + '/' + year + ' ' + hours + ':' + minutes + ':' + seconds);
+    // here we get the element with the id of "date" and change the content to the text variable we made above
+    return text;
   }
 }
